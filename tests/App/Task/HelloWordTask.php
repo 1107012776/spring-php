40aa619f43e20d3bdb37c8b32efab534db0bc135
linyushan <linyushan@nawang.cn>
2022-02-15 10:32:02 +0800

    v0.0.7

---
+++ tests/App/Task/HelloWordTask.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Task;
+use SpringPHP\Inter\TaskInterface;
+
+class HelloWordTask implements TaskInterface {
+    protected $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function before($taskId = 0, $workerIndex = 0)
+    {
+
+    }
+
+    public function run($taskId = 0, $workerIndex = 0)
+    {
+        echo __CLASS__.' task_id='.$taskId.' pid='.getmypid().'  '.var_export($this->data, true);
+    }
+
+    public function after($taskId = 0, $workerIndex = 0)
+    {
+
+    }
+
+    public function finish($taskId = 0, $workerIndex = 0)
+    {
+
+    }
+
+    public function onException(\Throwable $throwable, $arg = [])
+    {
+
+    }
+
+
+}
