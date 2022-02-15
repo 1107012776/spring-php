450a4d71625f3ed76312676ea70acc5839413e59
linyushan <linyushan@nawang.cn>
2022-02-15 12:55:31 +0800

    命名调整

--- src/Template/RenderInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SpringPHP\Template;
-interface RenderInterface
-{
-    public function render(string $template, ?array $data = null, ?array $options = null): ?string;
-
-    public function onException(\Throwable $throwable, $arg): string;
-}
