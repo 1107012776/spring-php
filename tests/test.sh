00661ec5822f6f63ec02a1449b0537e5e71e71c6
linyushan <linyushan@nawang.cn>
2022-02-16 15:21:14 +0800

    v0.0.12

---
+++ tests/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+curl http://127.0.0.1:7999/Index/index
+echo -e "\n"
+curl http://127.0.0.1:8098/Index/index
+echo -e "\n"
+curl http://127.0.0.1:8297/Index/index
+echo -e "\n"
+
+curl http://127.0.0.1:8297/Index/getContent
+echo -e "\n"
+
+curl http://127.0.0.1:8297/Index/rawContent
+echo -e "\n"
