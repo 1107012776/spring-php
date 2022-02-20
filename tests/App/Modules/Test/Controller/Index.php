b5584139137697e32da5a378516ace9706b6184d
linyushan <1107012776@qq.com>
2022-02-20 23:16:45 +0800

    socket and websocket 开发

--- tests/App/Modules/Test/Controller/Index.php
+++ tests/App/Modules/Test/Controller/Index.php
@@ -17,5 +17,18 @@ class Index extends Controller
         ]);
     }
 
+    public function form(){
+        return Render::getInstance()->render("Test/Index/form", [
+            'msg' => __CLASS__,
+            'module_name' => $this->request->getModuleName()
+        ]);
+    }
+
+    public function upload(){
+        return [
+            'files' => var_export($this->request->files(), true),
+            'tmpfiles' => var_export($this->request->tmpfiles(), true)
+        ];
+    }
 
 }
