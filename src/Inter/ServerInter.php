259c1813a2bab607b09b77cdae437c9ee43a8dc1
linyushan <linyushan@nawang.cn>
2022-02-21 09:45:46 +0800

    备注修改

--- src/Inter/ServerInter.php
+++ src/Inter/ServerInter.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * SpringPHP file.
+ * @author linyushan  <1107012776@qq.com>
+ * @link https://www.developzhe.com/
+ * @package https://github.com/1107012776/spring-php
+ * @license https://github.com/1107012776/spring-php/blob/main/LICENSE
+ */
 namespace SpringPHP\Inter;
 
 interface ServerInter
--- src/Template/Render.php
+++ src/Template/Render.php
@@ -1,5 +1,12 @@
 <?php
 /**
+ * SpringPHP file.
+ * @author linyushan  <1107012776@qq.com>
+ * @link https://www.developzhe.com/
+ * @package https://github.com/1107012776/spring-php
+ * @license https://github.com/1107012776/spring-php/blob/main/LICENSE
+ */
+/**
  * 模板
  */
 
